06e561469d4333ff462aa110e6cd9c3fd0ffe9ee
RickyJohansson <Rickjoh91@hotmail.com>
2023-03-08 14:10:59 +0100

    header positioning

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,28 @@
 import Search from '../search/Search'
+import kugghjul from '../../images/kugghjul.svg';
 import './header.scss'
+import { useState } from 'react';
 
 const Header = () => {
+
+    const [ overlay, setOverlay ] = useState<boolean>(false);
+
+    const openOverlay = () => {
+        setOverlay(!overlay);
+    }
+
     return (
         <header>
-            <button>Kuggis</button>
+            <img onClick={openOverlay} className="gearwheel_icon" src={ kugghjul } alt="" />
             <section>
-                <h1>Game Name - karaktär namn - visas ej i desktop vy</h1>
-                <h2>/breadcrumb/goes/here</h2>
+                <h1>Game Name</h1>
+                <h3>/Game/GameName</h3>
             </section>
             <Search />
-            <nav className='header_overlay'>
+            <nav className={'header_overlay' + `-${ overlay }`}>
                 <ul>
                     <li>
-                        <img src="" alt="" />
+                        <img src={ kugghjul } alt="" />
                         <p>Options</p>
                     </li>
                     <li>
