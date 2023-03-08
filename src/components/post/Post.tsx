18aa758139bf939eb87272c698db1f4524d39fa6
RickyJohansson <Rickjoh91@hotmail.com>
2023-03-08 16:57:08 +0100

    post section and post css
    Co-Authored-By: Emil Arvidsson <92728538+KarlEmilArvid@users.noreply.github.com>

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,11 +1,13 @@
 import './Post.scss'
+import star from '../../images/star.svg';
+import accordionArrow from '../../images/accordionArrow.svg';
 
 const Post = () => {
     return (
         <li>
             <h3></h3>
-            <img src="" alt="down-button" />
-            <img src="" alt="star" />
+            <img src={accordionArrow} alt="down-button" />
+            <img src={star} alt="star" />
             {/**dragspels funktion goes here */}
         </li>
     )
