4de2cc0f55b3589c6cf221b109706191d11c1e2a
RickyJohansson <Rickjoh91@hotmail.com>
2023-03-10 15:17:25 +0100

    Create post overlay open/close
    Co-Authored-By: Emil Arvidsson <92728538+KarlEmilArvid@users.noreply.github.com>

--- src/components/postOverlay/PostOverlay.tsx
+++ src/components/postOverlay/PostOverlay.tsx
@@ -1,13 +1,30 @@
+import { useState, useEffect } from 'react';
 import './PostOverlay.scss'
 
-const PostOverlay = () => {
+type Props = {
+    overlay: boolean;
+    setOverlay: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const PostOverlay = ({ overlay, setOverlay }: Props) => {
+    
+    const [ showOverlay, setShowOverlay ] = useState<string>('post-overlay-wrapper');
+
+    useEffect(() => {
+        overlay ? setShowOverlay('post-overlay-wrapper-show') : setShowOverlay('post-overlay-wrapper');
+    }, []);
+
+    const closeOverlay = () => {
+        setOverlay(false);
+    }
+
     return (
-        <div className='post-overlay-wrapper'>
-            <button>X</button>
-            <section>
+        <div className={ showOverlay }>
+            <button className="close-button" onClick={ closeOverlay }>X</button>
+            <section className="create-post-container">
                 <h2>Create/Edit Post</h2>
                 <input type="text" placeholder='post name:' />
-                <input type="text" placeholder='post text goes here...' />
+                <textarea className="text-input" placeholder='post text goes here...' />
                 <button>Public/Private</button>
                 <button>Submit/Save</button>
             </section>
